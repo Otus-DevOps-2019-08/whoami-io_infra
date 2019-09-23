5a2c5aa7fa6e0a0f352c524021e954c74b8ffc86
Marina Kolesnik <nn575073@gmail.com>
2019-09-23 13:03:38 +0300

    update startup_script

--- startup_script.sh
+++ startup_script.sh
@@ -2,25 +2,51 @@
 
 cd /home/appuser
 
-sudo git clone https://github.com/Otus-DevOps-2019-08/whoami-io_infra.git
-cd whoami-io_infra
-sudo git checkout cloud-testapp
+# install dependencies 
 
-./install_ruby.sh
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
 if [ $? -ne 0 ]; then
-  echo "Failed to install ruby"
+  echo "Failed to install rube and bundler"
   exit 1
 fi
 
-./install_mongodb.sh
+# install db
+
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add
+
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+sudo systemctl start mongod
 if [ $? -ne 0 ]; then
-  echo "Failed to install mongo
+  echo "Failed to start mongod"
   exit 1
 fi
 
-sudo ./deploy.sh
+sudo systemctl enable mongod
 if [ $? -ne 0 ]; then
-  echo "Failed to deploy reddit"
+  echo "Failed to enable mongod"
+  exit 1
+fi
+
+# deploy app
+
+git clone -b monolith https://github.com/express42/reddit.git
+
+cd reddit
+bundle install
+if [ $? -ne 0 ]; then
+  echo "Failed to install with bundle"
+  exit 1
+fi
+
+puma -d
+if [ $? -ne 0 ]; then
+  echo "Failed to start puma in detached mode"
   exit 1
 fi
 
